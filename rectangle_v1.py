b25380fe885d450b2126d74b487bcc22016dee96
Ohn08 <202280234@psu.palawan.edu.ph>
2023-12-22 15:49:27 +0800

    Add docstring & fix codes for readability

---
+++ rectangle_v1.py
@@ -0,0 +1,35 @@
+def calculate_area(length: float, width: float):
+    return length * width
+'''
+Calculate the area of the rectangle by multiplying the length and width b.
+Parameters:
+    length (float): The length of rectangle
+    width (float): The width of the rectangle
+'''
+# [Put Docstring in this function]
+def calculate_perimeter(length: float, width: float):
+    return 2 * (length + width)
+'''
+Multiplying the sum of the length and width of the triangle
+gives you the total length of the four sides of the rectangle, 
+which is the definition of parameter.
+'''
+
+def main():
+    length = float(input("Enter the length of the rectangle: "))
+    width = float(input("Enter the width of the rectangle: "))
+    '''
+    The users need to put their desired lenght and width
+    to calculate the area and parameter.
+    '''
+    area = calculate_area(length, width)
+    perimeter = calculate_perimeter(length, width)
+    '''
+    In this part the outcome or 
+    the result will be printed.
+    '''
+    print(f"Area of the rectangle: {area}")
+    print(f"Perimeter of the rectangle: {perimeter}")
+
+if __name__ == "__main__":
+    main()
